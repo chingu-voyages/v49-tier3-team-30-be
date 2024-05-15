8ad46a7ed96148f3612dc627151b8271b8c72ff8
Im-Humor <orangite@gmail.com>
2024-05-15 11:11:44 -0700

    correct middleware issue by adding next

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ require("dotenv").config();
 // loading router modules
 const userRouter = require("./routes/userRouter");
 
-app.use("/user", userRouter);
-
 // express middleware to parse requests with JSON payloads
 app.use(express.json());
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
 	res.json({ body: "Hello, world!" });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,6 +9,7 @@ const logUserParams = (req, res, next) => {
 	if (req.params) {
 		console.log(req.params);
 	}
+	next();
 };
 
 router.use(logUserParams);
@@ -16,5 +17,6 @@ router.use(logUserParams);
 // example router handler for https://localhost:3000/user/
 // reponds with id param if set
 router.get("/", userController.userTest);
+router.get("/:userId", userController.userTest);
 
 module.exports = router;
