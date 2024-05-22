15514009d257ee1343abf8be9bc07a6622f9fbe4
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2024-05-22 00:56:58 -0700

    Changed the syntax from import to require for cors as we are not using es6

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import cors from "cors";
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
