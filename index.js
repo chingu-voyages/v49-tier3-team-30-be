9ce4f9b8ed5e73cc384d9a4cbdffb2b16a54433d
Im-Humor <orangite@gmail.com>
2024-05-28 11:01:49 -0700

    update then syntax to async/await for mongoose connection

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI || "";
+
+// connecting to Mongoose server
+try {
+	await mongoose.connect(MONGO_URI);
+	console.log("[SERVER]: Database is connected");
+} catch (err) {
+	console.log("[ERROR]: Database is not connected");
+}
+
+// CORS to connect with client side
+const corsOptions = {
+	origin: process.env.CLIENT_SIDE,
+	credentials: true,
+};
 
 // loading router modules
 const userRouter = require("./routes/userRouter");
 
 // express middleware to parse requests with JSON payloads
 app.use(express.json());
-
+app.use(cors(corsOptions));
 app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
