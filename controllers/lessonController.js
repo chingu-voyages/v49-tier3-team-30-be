5b400e582753510143b560f7f1c38fdf00aa64c8
Im-Humor <orangite@gmail.com>
2024-05-29 14:45:44 -0700

    update lesson schema to remove notedata and update createLesson WIP function

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -1,3 +1,9 @@
-exports.userTest = (req, res) => {
-	res.send({ body: "User request received." });
+exports.createLesson = (req, res) => {
+	res.json({
+		parentCourse: 1,
+		name: "HTML",
+		desc: "This is the HTML course",
+		urls: ["www.htlm.com"],
+		isCompleted: false,
+	});
 };
--- routes/lessonRouter.js
+++ routes/lessonRouter.js
@@ -4,19 +4,9 @@ const router = express.Router();
 // import route-handler callback functions
 const lessonController = require("../controllers/lessonController");
 
-// example middleware logging router params if exists on every request
-const logUserParams = (req, res, next) => {
-	if (req.params) {
-		console.log(req.params);
-	}
-	next();
-};
-
-router.use(logUserParams);
-
 // example router handler for https://localhost:3000/user/
 // reponds with id param if set
-router.get("/", lessonController.userTest);
-router.get("/:userId", lessonController.userTest);
+
+router.post("/create", lessonController.createLesson);
 
 module.exports = router;
