f5ede94e327b72b5279c3f0287560bd5ca1d1de0
Im-Humor <orangite@gmail.com>
2024-05-29 15:46:32 -0700

    update createLesson with minor validation

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -4,9 +4,16 @@ const Lesson = require("../models/lesson");
 
 exports.createLesson = async (req, res) => {
 	console.log(req.body);
-	const newUser = new Lesson(req.body);
+	const { parentCourse, name, desc, urls } = req.body;
+	if (!parentCourse) {
+		return res.status(400).json({ message: "Parent course ID required" });
+	}
+	if (!name) {
+		return res.status(400).json({ message: "Course name required" });
+	}
+	const newLesson = new Lesson({ parentCourse, name, desc, urls });
 	try {
-		await newUser.save();
+		await newLesson.save();
 	} catch (err) {
 		console.error(err);
 	}
