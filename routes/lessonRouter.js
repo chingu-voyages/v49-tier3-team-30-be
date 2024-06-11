e65574c45aa43add27c71318ea69fa327933bb6b
NadiaPia <206i417@gmail.com>
2024-06-11 15:56:46 -0700

    added updating completed lessons in the user's db

--- routes/lessonRouter.js
+++ routes/lessonRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const {validateToken} = require("../middlewares/AuthMiddlewares")
+
 
 // import route-handler callback functions
 const lessonController = require("../controllers/lessonController");
@@ -8,7 +10,7 @@ const lessonController = require("../controllers/lessonController");
 // reponds with id param if set
 
 router.post("/create", lessonController.createLesson);
-router.put("/checkbox/:id", lessonController.handleCheckbox);
+router.put("/checkbox/:id", validateToken, lessonController.handleCheckbox);
 router.get("/:id", lessonController.getLessonDetails);
 
 
